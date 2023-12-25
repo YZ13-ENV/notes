58dce4f1e1b90fa6995987bbe06e96479595239b
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-12-25 11:09:31 +0500

    added condition for add new note button

--- components/widgets/header.tsx
+++ components/widgets/header.tsx
@@ -2,14 +2,20 @@ import Link from "next/link"
 import NotesMark from "../shared/notes-mark"
 import User from "../shared/user"
 import { Button } from "../ui/button"
+import { cookies } from "next/headers"
 
 const Header = () => {
+    const cookiesList = cookies()
+    const uidCookie = cookiesList.get('uid')
+    const uid = uidCookie ? uidCookie.value : null
     return (
         <header className='w-full h-16 shrink-0 border-b bg-card'>
             <div className='w-full max-w-7xl mx-auto flex items-center justify-between px-6 h-full'>
                 <Link href='/'><NotesMark /></Link>
                 <div className="w-fit h-fit flex items-center gap-4">
-                    <Button size='sm'><Link href='/note/new'>Добавить</Link></Button>
+                    {
+                        uid && <Button size='sm'><Link href='/note/new'>Добавить</Link></Button>
+                    }
                     <User />
                 </div>
             </div>
